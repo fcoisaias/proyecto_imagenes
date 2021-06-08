c49de1700ce2ac021c66878bc087275950fa4ee2
fcoisaias <65827476+fcoisaias@users.noreply.github.com>
2021-06-08 15:40:14 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
-let Mi_Imagen = images.iconImage(IconNames.Heart)
+input.onButtonPressed(Button.A, function () {
+    lugar = randint(0, 3)
+    basic.showNumber(lugar + 1)
+    if (lugar == 0) {
+        Mi_Imagen[lugar].showImage(0)
+        basic.pause(500)
+    } else if (lugar == 1) {
+        Mi_Imagen[lugar].showImage(0)
+        basic.pause(500)
+    } else if (lugar == 2) {
+        Mi_Imagen[lugar].showImage(0)
+        basic.pause(500)
+    } else {
+        Mi_Imagen[lugar].showImage(0)
+        basic.pause(500)
+    }
+    basic.clearScreen()
+})
+let lugar = 0
+let Mi_Imagen: Image[] = []
+Mi_Imagen = [images.iconImage(IconNames.Heart), images.iconImage(IconNames.Tortoise), images.iconImage(IconNames.Silly), images.iconImage(IconNames.Pitchfork)]
 basic.forever(function () {
-    Mi_Imagen.scrollImage(1, 500)
+	
 })
